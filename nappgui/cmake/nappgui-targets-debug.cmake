ec123a9303749a6dd49bfedde2e11b63740000da
Avila Khadhibyan <khavl2103@gmail.com>
2024-06-08 11:38:22 +0700

    Initial commit

---
+++ nappgui/cmake/nappgui-targets-debug.cmake
@@ -0,0 +1,271 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Debug".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "nappgui::sewer" for configuration "Debug"
+set_property(TARGET nappgui::sewer APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(nappgui::sewer PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "C;CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libsewer.a"
+  )
+
+list(APPEND _cmake_import_check_targets nappgui::sewer )
+list(APPEND _cmake_import_check_files_for_nappgui::sewer "${_IMPORT_PREFIX}/lib/libsewer.a" )
+
+# Import target "nappgui::osbs" for configuration "Debug"
+set_property(TARGET nappgui::osbs APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(nappgui::osbs PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "C"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libosbs.a"
+  )
+
+list(APPEND _cmake_import_check_targets nappgui::osbs )
+list(APPEND _cmake_import_check_files_for_nappgui::osbs "${_IMPORT_PREFIX}/lib/libosbs.a" )
+
+# Import target "nappgui::core" for configuration "Debug"
+set_property(TARGET nappgui::core APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(nappgui::core PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "C;CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libcore.a"
+  )
+
+list(APPEND _cmake_import_check_targets nappgui::core )
+list(APPEND _cmake_import_check_files_for_nappgui::core "${_IMPORT_PREFIX}/lib/libcore.a" )
+
+# Import target "nappgui::geom2d" for configuration "Debug"
+set_property(TARGET nappgui::geom2d APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(nappgui::geom2d PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libgeom2d.a"
+  )
+
+list(APPEND _cmake_import_check_targets nappgui::geom2d )
+list(APPEND _cmake_import_check_files_for_nappgui::geom2d "${_IMPORT_PREFIX}/lib/libgeom2d.a" )
+
+# Import target "nappgui::draw2d" for configuration "Debug"
+set_property(TARGET nappgui::draw2d APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(nappgui::draw2d PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "C;CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libdraw2d.a"
+  )
+
+list(APPEND _cmake_import_check_targets nappgui::draw2d )
+list(APPEND _cmake_import_check_files_for_nappgui::draw2d "${_IMPORT_PREFIX}/lib/libdraw2d.a" )
+
+# Import target "nappgui::osgui" for configuration "Debug"
+set_property(TARGET nappgui::osgui APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(nappgui::osgui PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "C;CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libosgui.a"
+  )
+
+list(APPEND _cmake_import_check_targets nappgui::osgui )
+list(APPEND _cmake_import_check_files_for_nappgui::osgui "${_IMPORT_PREFIX}/lib/libosgui.a" )
+
+# Import target "nappgui::gui" for configuration "Debug"
+set_property(TARGET nappgui::gui APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(nappgui::gui PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "C;CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libgui.a"
+  )
+
+list(APPEND _cmake_import_check_targets nappgui::gui )
+list(APPEND _cmake_import_check_files_for_nappgui::gui "${_IMPORT_PREFIX}/lib/libgui.a" )
+
+# Import target "nappgui::osapp" for configuration "Debug"
+set_property(TARGET nappgui::osapp APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(nappgui::osapp PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "C;CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libosapp.a"
+  )
+
+list(APPEND _cmake_import_check_targets nappgui::osapp )
+list(APPEND _cmake_import_check_files_for_nappgui::osapp "${_IMPORT_PREFIX}/lib/libosapp.a" )
+
+# Import target "nappgui::inet" for configuration "Debug"
+set_property(TARGET nappgui::inet APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(nappgui::inet PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "C;CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libinet.a"
+  )
+
+list(APPEND _cmake_import_check_targets nappgui::inet )
+list(APPEND _cmake_import_check_files_for_nappgui::inet "${_IMPORT_PREFIX}/lib/libinet.a" )
+
+# Import target "nappgui::nrc" for configuration "Debug"
+set_property(TARGET nappgui::nrc APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(nappgui::nrc PROPERTIES
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/nrc.exe"
+  )
+
+list(APPEND _cmake_import_check_targets nappgui::nrc )
+list(APPEND _cmake_import_check_files_for_nappgui::nrc "${_IMPORT_PREFIX}/bin/nrc.exe" )
+
+# Import target "nappgui::Bode" for configuration "Debug"
+set_property(TARGET nappgui::Bode APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(nappgui::Bode PROPERTIES
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/Bode.exe"
+  )
+
+list(APPEND _cmake_import_check_targets nappgui::Bode )
+list(APPEND _cmake_import_check_files_for_nappgui::Bode "${_IMPORT_PREFIX}/bin/Bode.exe" )
+
+# Import target "nappgui::Bricks" for configuration "Debug"
+set_property(TARGET nappgui::Bricks APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(nappgui::Bricks PROPERTIES
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/Bricks.exe"
+  )
+
+list(APPEND _cmake_import_check_targets nappgui::Bricks )
+list(APPEND _cmake_import_check_files_for_nappgui::Bricks "${_IMPORT_PREFIX}/bin/Bricks.exe" )
+
+# Import target "nappgui::casino" for configuration "Debug"
+set_property(TARGET nappgui::casino APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(nappgui::casino PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "C"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libcasino.a"
+  )
+
+list(APPEND _cmake_import_check_targets nappgui::casino )
+list(APPEND _cmake_import_check_files_for_nappgui::casino "${_IMPORT_PREFIX}/lib/libcasino.a" )
+
+# Import target "nappgui::Col2dHello" for configuration "Debug"
+set_property(TARGET nappgui::Col2dHello APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(nappgui::Col2dHello PROPERTIES
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/Col2dHello.exe"
+  )
+
+list(APPEND _cmake_import_check_targets nappgui::Col2dHello )
+list(APPEND _cmake_import_check_files_for_nappgui::Col2dHello "${_IMPORT_PREFIX}/bin/Col2dHello.exe" )
+
+# Import target "nappgui::ColorView" for configuration "Debug"
+set_property(TARGET nappgui::ColorView APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(nappgui::ColorView PROPERTIES
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/ColorView.exe"
+  )
+
+list(APPEND _cmake_import_check_targets nappgui::ColorView )
+list(APPEND _cmake_import_check_files_for_nappgui::ColorView "${_IMPORT_PREFIX}/bin/ColorView.exe" )
+
+# Import target "nappgui::Dice" for configuration "Debug"
+set_property(TARGET nappgui::Dice APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(nappgui::Dice PROPERTIES
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/Dice.exe"
+  )
+
+list(APPEND _cmake_import_check_targets nappgui::Dice )
+list(APPEND _cmake_import_check_files_for_nappgui::Dice "${_IMPORT_PREFIX}/bin/Dice.exe" )
+
+# Import target "nappgui::Die" for configuration "Debug"
+set_property(TARGET nappgui::Die APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(nappgui::Die PROPERTIES
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/Die.exe"
+  )
+
+list(APPEND _cmake_import_check_targets nappgui::Die )
+list(APPEND _cmake_import_check_files_for_nappgui::Die "${_IMPORT_PREFIX}/bin/Die.exe" )
+
+# Import target "nappgui::DrawBig" for configuration "Debug"
+set_property(TARGET nappgui::DrawBig APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(nappgui::DrawBig PROPERTIES
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/DrawBig.exe"
+  )
+
+list(APPEND _cmake_import_check_targets nappgui::DrawBig )
+list(APPEND _cmake_import_check_files_for_nappgui::DrawBig "${_IMPORT_PREFIX}/bin/DrawBig.exe" )
+
+# Import target "nappgui::DrawHello" for configuration "Debug"
+set_property(TARGET nappgui::DrawHello APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(nappgui::DrawHello PROPERTIES
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/DrawHello.exe"
+  )
+
+list(APPEND _cmake_import_check_targets nappgui::DrawHello )
+list(APPEND _cmake_import_check_files_for_nappgui::DrawHello "${_IMPORT_PREFIX}/bin/DrawHello.exe" )
+
+# Import target "nappgui::DrawImg" for configuration "Debug"
+set_property(TARGET nappgui::DrawImg APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(nappgui::DrawImg PROPERTIES
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/DrawImg.exe"
+  )
+
+list(APPEND _cmake_import_check_targets nappgui::DrawImg )
+list(APPEND _cmake_import_check_files_for_nappgui::DrawImg "${_IMPORT_PREFIX}/bin/DrawImg.exe" )
+
+# Import target "nappgui::Fractals" for configuration "Debug"
+set_property(TARGET nappgui::Fractals APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(nappgui::Fractals PROPERTIES
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/Fractals.exe"
+  )
+
+list(APPEND _cmake_import_check_targets nappgui::Fractals )
+list(APPEND _cmake_import_check_files_for_nappgui::Fractals "${_IMPORT_PREFIX}/bin/Fractals.exe" )
+
+# Import target "nappgui::GuiHello" for configuration "Debug"
+set_property(TARGET nappgui::GuiHello APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(nappgui::GuiHello PROPERTIES
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/GuiHello.exe"
+  )
+
+list(APPEND _cmake_import_check_targets nappgui::GuiHello )
+list(APPEND _cmake_import_check_files_for_nappgui::GuiHello "${_IMPORT_PREFIX}/bin/GuiHello.exe" )
+
+# Import target "nappgui::HelloWorld" for configuration "Debug"
+set_property(TARGET nappgui::HelloWorld APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(nappgui::HelloWorld PROPERTIES
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/HelloWorld.exe"
+  )
+
+list(APPEND _cmake_import_check_targets nappgui::HelloWorld )
+list(APPEND _cmake_import_check_files_for_nappgui::HelloWorld "${_IMPORT_PREFIX}/bin/HelloWorld.exe" )
+
+# Import target "nappgui::HelloCpp" for configuration "Debug"
+set_property(TARGET nappgui::HelloCpp APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(nappgui::HelloCpp PROPERTIES
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/HelloCpp.exe"
+  )
+
+list(APPEND _cmake_import_check_targets nappgui::HelloCpp )
+list(APPEND _cmake_import_check_files_for_nappgui::HelloCpp "${_IMPORT_PREFIX}/bin/HelloCpp.exe" )
+
+# Import target "nappgui::htjson" for configuration "Debug"
+set_property(TARGET nappgui::htjson APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(nappgui::htjson PROPERTIES
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/htjson.exe"
+  )
+
+list(APPEND _cmake_import_check_targets nappgui::htjson )
+list(APPEND _cmake_import_check_files_for_nappgui::htjson "${_IMPORT_PREFIX}/bin/htjson.exe" )
+
+# Import target "nappgui::Products" for configuration "Debug"
+set_property(TARGET nappgui::Products APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(nappgui::Products PROPERTIES
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/Products.exe"
+  )
+
+list(APPEND _cmake_import_check_targets nappgui::Products )
+list(APPEND _cmake_import_check_files_for_nappgui::Products "${_IMPORT_PREFIX}/bin/Products.exe" )
+
+# Import target "nappgui::stlcmp" for configuration "Debug"
+set_property(TARGET nappgui::stlcmp APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(nappgui::stlcmp PROPERTIES
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/stlcmp.exe"
+  )
+
+list(APPEND _cmake_import_check_targets nappgui::stlcmp )
+list(APPEND _cmake_import_check_files_for_nappgui::stlcmp "${_IMPORT_PREFIX}/bin/stlcmp.exe" )
+
+# Import target "nappgui::UrlImg" for configuration "Debug"
+set_property(TARGET nappgui::UrlImg APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(nappgui::UrlImg PROPERTIES
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/UrlImg.exe"
+  )
+
+list(APPEND _cmake_import_check_targets nappgui::UrlImg )
+list(APPEND _cmake_import_check_files_for_nappgui::UrlImg "${_IMPORT_PREFIX}/bin/UrlImg.exe" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
